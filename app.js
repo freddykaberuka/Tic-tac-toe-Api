bf564f334982418986c9a7d39e8600fd0513c39f
freddykaberuka <freddykaberuka2009@gmail.com>
2023-02-16 21:05:32 +0200

    Validate moves and display the result

--- app.js
+++ app.js
@@ -6,6 +6,51 @@ var isMovePossible = function (board) {
     var oCount = board.split('o').length - 1;
     return xCount === oCount || xCount === oCount + 1;
 };
+function getValidMoves(board) {
+    var moves = board.split('').reduce(function (moves, cell, i) {
+        return cell === '-' ? moves.concat(i) : moves;
+    }, []);
+    console.log('valid moves:', moves);
+    return moves;
+}
+function average(board, player, maxPlayer, depth, maxDepth) {
+    if (depth === void 0) { depth = 0; }
+    if (maxDepth === void 0) { maxDepth = 5; }
+    if (depth >= maxDepth) {
+        console.log('depth:', depth, 'score:', 0);
+        return [0, null];
+    }
+    var nextPlayer = player === 'x' ? 'o' : 'x';
+    var validMoves = getValidMoves(board);
+    if (maxPlayer) {
+        var bestScore = Number.NEGATIVE_INFINITY;
+        var bestMove = void 0;
+        for (var _i = 0, validMoves_1 = validMoves; _i < validMoves_1.length; _i++) {
+            var move = validMoves_1[_i];
+            var _a = average(board.substr(0, move) + 'o' + board.substr(move + 1), nextPlayer, false, depth + 1, maxDepth), score = _a[0], _ = _a[1];
+            console.log('depth:', depth, 'score:', score);
+            if (score > bestScore) {
+                bestScore = score;
+                bestMove = move;
+            }
+        }
+        return [bestScore, bestMove];
+    }
+    else {
+        var bestScore = Number.POSITIVE_INFINITY;
+        var bestMove = void 0;
+        for (var _b = 0, validMoves_2 = validMoves; _b < validMoves_2.length; _b++) {
+            var move = validMoves_2[_b];
+            var _c = average(board.substr(0, move) + 'x' + board.substr(move + 1), nextPlayer, true, depth + 1, maxDepth), score = _c[0], _ = _c[1];
+            console.log('depth:', depth, 'score:', score);
+            if (score < bestScore) {
+                bestScore = score;
+                bestMove = move;
+            }
+        }
+        return [bestScore, bestMove];
+    }
+}
 var app = express();
 var port = 3000;
 app.get('/', function (req, res) {
@@ -13,10 +58,13 @@ app.get('/', function (req, res) {
 });
 app.get('/play', function (req, res) {
     var board = req.query.board;
-    console.log("Board: ".concat(Board));
+    console.log("board: ".concat(board));
     if (!/^[x\-o]{9}$/.test(board) || !isMovePossible(board)) {
         return res.sendStatus(400);
     }
+    var _a = average(board, 'x', true), _ = _a[0], move = _a[1];
+    var newBoard = board.substr(0, move) + 'o' + board.substr(move + 1);
+    res.send(newBoard);
 });
 app.listen(port, function () {
     console.log("server is listening at http://localhost:".concat(port));
--- app.ts
+++ app.ts
@@ -3,27 +3,80 @@ import type { Request, Response } from 'express';
 
 type Board = string;
 
-const isMovePossible=(board: Board): boolean =>{
-  const xCount = board.split('x').length - 1;
-  const oCount = board.split('o').length - 1;
-  return xCount === oCount || xCount === oCount + 1;
+const isMovePossible = (board: Board): boolean => {
+    const xCount = board.split('x').length - 1;
+    const oCount = board.split('o').length - 1;
+    return xCount === oCount || xCount === oCount + 1;
 }
 
+
+
+function getValidMoves(board: Board): number[] {
+  const moves = board.split('').reduce((moves, cell, i) =>
+    cell === '-' ? moves.concat(i) : moves
+  , []);
+  console.log('valid moves:', moves);
+  return moves;
+}
+
+
+function average(board: Board, player: string, maxPlayer: boolean, depth: number = 0, maxDepth: number = 5): [number, number] {
+  if (depth >= maxDepth) {
+    console.log('depth:', depth, 'score:', 0);
+    return [0, null];
+  }
+  const nextPlayer = player === 'x' ? 'o' : 'x';
+  const validMoves = getValidMoves(board);
+  if (maxPlayer) {
+    let bestScore = Number.NEGATIVE_INFINITY;
+    let bestMove: number;
+    for (const move of validMoves) {
+      const [score, _] = average(
+        board.substr(0, move) + 'o' + board.substr(move + 1),
+        nextPlayer, false, depth + 1, maxDepth
+      );
+      console.log('depth:', depth, 'score:', score);
+      if (score > bestScore) {
+        bestScore = score;
+        bestMove = move;
+      }
+    }
+    return [bestScore, bestMove];
+  } else {
+    let bestScore = Number.POSITIVE_INFINITY;
+    let bestMove: number;
+    for (const move of validMoves) {
+      const [score, _] = average(
+        board.substr(0, move) + 'x' + board.substr(move + 1),
+        nextPlayer, true, depth + 1, maxDepth
+      );
+      console.log('depth:', depth, 'score:', score);
+      if (score < bestScore) {
+        bestScore = score;
+        bestMove = move;
+      }
+    }
+    return [bestScore, bestMove];
+  }
+}
 const app = express();
 const port = 3000;
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Tic tac toe game')
 });
 
 app.get('/play', (req: Request, res: Response) => {
-  const board = req.query.board as Board;
-  console.log(`board: ${board}`);
-  
-  if (!/^[x\-o]{9}$/.test(board) || !isMovePossible(board)) {
-    return res.sendStatus(400);
-  }
+    const board = req.query.board as Board;
+    console.log(`board: ${board}`);
+
+    if (!/^[x\-o]{9}$/.test(board) || !isMovePossible(board)) {
+        return res.sendStatus(400);
+    }
+    const [_, move] = average(board, 'x', true);
+    const newBoard = board.substr(0, move) + 'o' + board.substr(move + 1);
+    res.send(newBoard);
 });
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server is listening at http://localhost:${port}`);
 });

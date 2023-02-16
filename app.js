b047b0a2abe8c0c642fc105829bbfdaa99c754cc
freddykaberuka <freddykaberuka2009@gmail.com>
2023-02-16 20:39:53 +0200

    Verify the move possibility and Add play Endpoint

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+"use strict";
+exports.__esModule = true;
 var express = require('express');
+var isMovePossible = function (board) {
+    var xCount = board.split('x').length - 1;
+    var oCount = board.split('o').length - 1;
+    return xCount === oCount || xCount === oCount + 1;
+};
 var app = express();
 var port = 3000;
 app.get('/', function (req, res) {
-    res.send('gaming app');
+    res.send('Tic tac toe game');
+});
+app.get('/play', function (req, res) {
+    var board = req.query.board;
+    console.log("Board: ".concat(Board));
+    if (!/^[x\-o]{9}$/.test(board) || !isMovePossible(board)) {
+        return res.sendStatus(400);
+    }
 });
 app.listen(port, function () {
     console.log("server is listening at http://localhost:".concat(port));
--- app.ts
+++ app.ts
@@ -1,11 +1,29 @@
 const express = require('express')
+import type { Request, Response } from 'express';
+
+type Board = string;
+
+const isMovePossible=(board: Board): boolean =>{
+  const xCount = board.split('x').length - 1;
+  const oCount = board.split('o').length - 1;
+  return xCount === oCount || xCount === oCount + 1;
+}
 
 const app = express();
 const port = 3000;
 
 app.get('/',(req,res)=>{
     res.send('Tic tac toe game')
-})
+});
+
+app.get('/play', (req: Request, res: Response) => {
+  const board = req.query.board as Board;
+  console.log(`board: ${board}`);
+  
+  if (!/^[x\-o]{9}$/.test(board) || !isMovePossible(board)) {
+    return res.sendStatus(400);
+  }
+});
 app.listen(port,()=>{
     console.log(`server is listening at http://localhost:${port}`);
 });
